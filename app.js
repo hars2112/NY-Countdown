891ba4caad8ce385b731983971f2c067d0bb675a
Hector_A_Rodriguez_S <rshector@gmail.com>
2021-06-23 23:56:30 -0400

    app.js con problemas

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const hours = document.querySelectorAll('hours');
 const minutes = document.querySelectorAll('minutes');
 const seconds = document.querySelectorAll('seconds');
 
-//set the currentdate in order to get newYearTime
+//set the currentDate in order to get newYearTime
 const currentYear = new Date().getFullYear();
 
 //set the date we're counting down to
@@ -14,6 +14,16 @@ ${currentYear + 1} 00:00:00`);
  function updateCountdownTime(){
 const currentTime = new Date();
 const diff = newYearTime - currentTime;
-console.log(diff);
+
+const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+const m = Math.floor(diff / 1000 / 60) % 60;
+const s = Math.floor(diff / 1000) % 60;
+
+days.innerHTML = d;
+hours.innerHTML = h < ? '0' + h : h;
+minutes.innerHTML = m < ? '0' + m : m;
+seconds.innerHTML = s < ? '0' + s : s;
+
  }
- updateCountdownTime();
+ setInterval(updateCountdownTime,1000);
